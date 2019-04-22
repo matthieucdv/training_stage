24011ebc24945c18fe24d13d388deb453b0c39ea
Matthieu Colin de Verdière <matthieu.cdv@gmail.com>
2019-04-22 21:36:31 +0200

    [LOC_AGILE] j'ai retiré le print du milieu

--- file1.py
+++ file1.py
@@ -1,2 +1,2 @@
 print("file 1 ohé")
-print("J'ai créé une nouvelle branche et je peux faire mes modifications tranquillement !")
+#print("J'ai créé une nouvelle branche et je peux faire mes modifications tranquillement !")
